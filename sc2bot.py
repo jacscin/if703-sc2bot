116e2543609e3123f05da5543ccf24cf65bb5874
João Antônio <jacs@cin.ufpe.br>
2020-11-28 20:04:43 -0300

    Scouting implemented and expanse code refactored

--- sc2bot.py
+++ sc2bot.py
@@ -15,6 +15,8 @@ class SC2Bot(sc2.BotAI):
     def __init__(self):
         self.iterbymin = 168
         self.assigned_queens = {}
+        self.scouts = {}
+        self.army_command = 0 #0: idle, 1: attack, 2: defend
 
     def select_target(self) -> Point2:
         if self.enemy_structures:
@@ -22,8 +24,10 @@ class SC2Bot(sc2.BotAI):
         return self.enemy_start_locations[0]
 
     async def on_step(self, iteration):
+        self.iteration = iteration
         await self.worker_manager()
         await self.production_manager()
+        await self.combat_manager()
 
     async def worker_manager(self):
         await self.distribute_workers()
@@ -34,6 +38,7 @@ class SC2Bot(sc2.BotAI):
             and self.can_afford(UnitTypeId.OVERLORD)
         ):
             self.train(UnitTypeId.OVERLORD)
+
         if self.can_afford(UnitTypeId.DRONE) and self.supply_workers < 20:
             self.train(UnitTypeId.DRONE)
         
@@ -58,20 +63,46 @@ class SC2Bot(sc2.BotAI):
             if queen.energy >= 25:
                 queen(AbilityId.EFFECT_INJECTLARVA, self.townhalls.first)
 
-
-    async def information_manager(self):
-        pass
-
     async def production_manager(self):
         await self.building_manager()
 
+    async def combat_manager(self):
+        # Attack
+        if(self.army_command == 1):
+            pass
+
+        # Defend
+        if(self.army_command == 2):
+            pass
+
     async def strategy_manager(self):
-        pass
+        # Scouting
+        to_be_removed = []
+        for scout in self.scouts:
+            if scout not in [unit.tag for unit in self.units]:
+                to_be_removed.append(scout)
+
+        for scout in to_be_removed:
+            del self.scouts[scout]
+            
+        if self.units(UnitTypeId.OVERLORD).idle.amount > 0:
+            for location in self.enemy_start_locations[:3]:
+                for scout in self.units(UnitTypeId.OVERLORD).idle[:3]:
+                    if location not in self.scouts.values():
+                        if scout.tag not in self.scouts:
+                            await self.do(scout.move(location))
+                            self.scouts[scout.tag] = location
+        self.army_command = 0
+
+        # Attacking
+        if(False):
+            self.army_command = 1
+
+        # Defending
+        if(False):
+            self.army_command = 2
     
     async def map_grid(self):
-        pass
-
-    async def combat_manager(self):
         pass
 
     async def building_manager(self):
@@ -84,26 +115,13 @@ class SC2Bot(sc2.BotAI):
             target: Unit = self.vespene_geyser.closest_to(drone)
             drone.build_gas(target)
 
-        with suppress(AssertionError):
-            if (
-                self.townhalls.amount < 2 
-                and (self.can_afford(UnitTypeId.HATCHERY) 
-                and self.units(UnitTypeId.DRONE).amount > 5) 
-                and self.already_pending(UnitTypeId.HATCHERY) < 1):
-                    planned_hatch_locations: Set[Point2] = {placeholder.position for placeholder in self.placeholders}
-                    my_structure_locations: Set[Point2] = {structure.position for structure in self.structures}
-                    enemy_structure_locations: Set[Point2] = {structure.position for structure in self.enemy_structures}
-                    blocked_locations: Set[
-                        Point2
-                    ] = my_structure_locations | planned_hatch_locations | enemy_structure_locations
-                    expansions = self.expansion_locations_list
-                    for exp_pos in expansions:
-                        if exp_pos in blocked_locations:
-                            continue
-                        for drone in self.workers.collecting:
-                            drone: Unit
-                            drone.build(UnitTypeId.HATCHERY, exp_pos)
-                            break
+        if (
+            self.townhalls.amount < 5
+            and self.can_afford(UnitTypeId.HATCHERY)
+            and self.townhalls.amount < self.iteration / self.iterbymin
+            and not self.already_pending(UnitTypeId.HATCHERY)
+        ):
+            self.expand_now()
         
         if (
             self.can_afford(UnitTypeId.SPAWNINGPOOL)
