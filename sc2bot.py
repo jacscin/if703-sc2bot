d9a0cf8063a51eadb92f43a86f4310d3115b4d02
Lavínia Paganini <laviniapaganini@gmail.com>
2020-11-28 23:31:52 -0300

    Add a attack

--- sc2bot.py
+++ sc2bot.py
@@ -25,7 +25,7 @@ class SC2Bot(sc2.BotAI):
         self.iteration = iteration
         await self.worker_manager()
         await self.production_manager()
-        await self.combat_manager()
+        await self.combat_manager(iteration)
 
     async def worker_manager(self):
         await self.distribute_workers()
@@ -37,14 +37,14 @@ class SC2Bot(sc2.BotAI):
         ):
             self.train(UnitTypeId.OVERLORD)
 
-        if self.can_afford(UnitTypeId.DRONE) and self.supply_workers < 80:
+        if self.can_afford(UnitTypeId.DRONE) and self.supply_workers < 15 * self.townhalls.amount:
             self.train(UnitTypeId.DRONE)
         
         if (
             self.can_afford(UnitTypeId.ZERGLING)
             and self.larva
             and self.supply_army + self.supply_workers > 20
-            and self.units(UnitTypeId.ZERGLING).amount < (6 * self.townhalls.amount)):
+            and self.units(UnitTypeId.ZERGLING).amount < (8 * self.townhalls.amount)):
                 self.larva.random.train(UnitTypeId.ZERGLING)
 
         if (
@@ -53,6 +53,9 @@ class SC2Bot(sc2.BotAI):
             and self.supply_army + self.supply_workers > 27
             and self.units(UnitTypeId.ROACH).amount < (5 * self.townhalls.amount)):
                 self.larva.random.train(UnitTypeId.ROACH)
+
+        if self.larva and self.can_afford(UnitTypeId.HYDRALISK) and self.structures(UnitTypeId.HYDRALISKDEN).ready:
+            self.larva.random.train(UnitTypeId.HYDRALISK)
         
         await self.queen_manager()
 
@@ -71,9 +74,25 @@ class SC2Bot(sc2.BotAI):
     async def production_manager(self):
         await self.building_manager()
 
-    async def combat_manager(self):
+    async def combat_manager(self, iteration):
         await self.strategy_manager()
         await self.upgrade_manager()
+
+        if self.units(UnitTypeId.HYDRALISK).amount >= 10 and iteration % 50 == 0:
+            for unit in self.units.of_type({UnitTypeId.ZERGLING, UnitTypeId.HYDRALISK}).idle:
+                unit.attack(self.select_target())
+
+        for unit in self.units:
+            if unit.type_id != UnitTypeId.OVERLORD:
+                closest_enemies = self.enemy_units.closer_than(5, unit)
+                if len(closest_enemies) > 0:
+                    target = random.choice(closest_enemies)
+                    attack_units : Units = self.units.of_type(
+                    {UnitTypeId.QUEEN, UnitTypeId.ZERGLING, UnitTypeId.HYDRALISK}
+                    ).prefer_idle
+                    closest_units = attack_units.n_closest_to_distance(target.position, 3, round(len(closest_enemies) * 1.25))
+                    for backup_unit in closest_units:
+                        backup_unit.attack(target)
 
         # Attack
         if(self.army_command == 1):
@@ -149,6 +168,18 @@ class SC2Bot(sc2.BotAI):
                     near=self.townhalls.first.position.towards(self.game_info.map_center, 7)
                     )
 
+        hydra_dens = self.structures(UnitTypeId.HYDRALISKDEN)
+        if hydra_dens:
+            for hydra_den in hydra_dens.ready.idle:
+                if self.already_pending_upgrade(UpgradeId.EVOLVEGROOVEDSPINES) == 0 and self.can_afford(
+                    UpgradeId.EVOLVEGROOVEDSPINES
+                ):
+                    hydra_den.research(UpgradeId.EVOLVEGROOVEDSPINES)
+                elif self.already_pending_upgrade(UpgradeId.EVOLVEMUSCULARAUGMENTS) == 0 and self.can_afford(
+                    UpgradeId.EVOLVEMUSCULARAUGMENTS
+                ):
+                    hydra_den.research(UpgradeId.EVOLVEMUSCULARAUGMENTS)
+
     async def building_manager(self):
         if (
             self.gas_buildings.amount + self.already_pending(UnitTypeId.EXTRACTOR) == 0
@@ -177,8 +208,13 @@ class SC2Bot(sc2.BotAI):
                     near=self.townhalls.first.position.towards(self.game_info.map_center, 5)
                     )
 
+        if self.townhalls(UnitTypeId.LAIR).ready:
+            if self.structures(UnitTypeId.HYDRALISKDEN).amount + self.already_pending(UnitTypeId.HYDRALISKDEN) == 0:
+                if self.can_afford(UnitTypeId.HYDRALISKDEN):
+                    await self.build(UnitTypeId.HYDRALISKDEN,  near=self.townhalls.first.position.towards(self.game_info.map_center, 5))
+
 run_game(
     maps.get("AcropolisLE"),
-    [Bot(Race.Zerg, SC2Bot()), Computer(Race.Terran, Difficulty.Hard)],
+    [Bot(Race.Zerg, SC2Bot()), Computer(Race.Terran, Difficulty.Easy)],
     realtime=False,
 )
